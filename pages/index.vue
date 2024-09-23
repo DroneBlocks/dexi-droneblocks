840e17721fa4c5da906e89eb0d1f777792104df0
Dennis Baldwin <dennisbaldwin@gmail.com>
2024-09-23 09:44:32 -0500

    Pi cam link and discord perm link

--- pages/index.vue
+++ pages/index.vue
@@ -13,19 +13,24 @@
         </NuxtLink>
       </li>
       <li>
+        <NuxtLink to="http://192.168.4.1:8080">
+          Pi Camera
+        </NuxtLink>
+      </li>
+      <li>
         <NuxtLink to="/droneblocks">
           DroneBlocks
         </NuxtLink>
       </li>
-      <li>VS Code</li>
-      <li>Terminal</li>
+      <!-- <li>VS Code</li>
+      <li>Terminal</li> -->
       <li>
         <NuxtLink to="https://github.com/droneblocks/dexi" target="_blank">
           GitHub
         </NuxtLink>
       </li>
       <li>
-        <NuxtLink to="https://discord.gg/SwyTXUvg" target="_blank">
+        <NuxtLink to="https://discord.gg/Wjw7wGf7Wn" target="_blank">
           Discord
         </NuxtLink>
       </li>
