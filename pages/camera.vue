10ca06ccbe3a51054b44c0bbeaa6f26bc60ac2b9
Dennis Baldwin <dennisbaldwin@gmail.com>
2025-03-24 12:54:28 -0500

    Camera stream view (#4)

---
+++ pages/camera.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h1>ROS Image Stream</h1>
+    <!-- Image tag to display the live image -->
+    <img v-if="imageData" :src="imageData" alt="ROS Image" width="640" height="480" />
+    <p v-else>Loading image...</p>
+  </div>
+</template>
+
+<script>
+import ROSLIB from "roslib";
+
+export default {
+  data() {
+    return {
+      imageData: null,  // This will store the base64-encoded image
+    };
+  },
+  mounted() {
+    // Connect to the ROSBridge WebSocket
+    const ros = new ROSLIB.Ros({
+      url: 'ws://192.168.6.178:9090',  // Adjust with your ROSBridge URL
+    });
+
+    ros.on('connection', function () {
+      console.log('connected')
+    });
+
+    // Subscribe to the /camera/image_raw topic
+    const imageTopic = new ROSLIB.Topic({
+      ros: ros,
+      name: '/cam0/image_raw/compressed',  // Your image topic
+      messageType: 'sensor_msgs/CompressedImage',
+    });
+
+    imageTopic.subscribe((message) => {      
+      // Set the base64 image string to be used in the img tag
+      this.imageData = `data:image/jpeg;base64,${message.data}`;
+      
+    });
+  },
+  beforeDestroy() {
+    // Unsubscribe from the topic when the component is destroyed
+    if (this.imageTopic) {
+      this.imageTopic.unsubscribe();
+    }
+  },
+  methods: {
+    // Convert uint8Array to base64 string
+    uint8ArrayToBase64(uint8Array) {
+      const binaryString = String.fromCharCode.apply(null, uint8Array);
+      return window.btoa(binaryString);
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Add any styling you want for the component */
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
