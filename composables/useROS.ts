37080e8ffdbdbfb5852c797447e98ec6f0185233
Dennis Baldwin <dennisbaldwin@gmail.com>
2025-05-31 18:36:40 -0500

    Fix hostname

--- composables/useROS.ts
+++ composables/useROS.ts
@@ -1,7 +1,7 @@
 export const useROS = () => {
   const getROSURL = (): string => {
     const hostname = process.client ? window.location.hostname : '192.168.4.1'
-    return `ws://127.0.0.1:9090`
+    return hostname
   }
 
   return {
